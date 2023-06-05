155d852b5994c7e6418bd10552add63fc8e141bf
Aaron <66452927+AaronNgoi@users.noreply.github.com>
2023-06-05 21:39:47 +1000

    NavBar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home';
+// import AllProducts from './pages/AllProducts';
+// import Tea from './pages/Tea';
+// import Accessories from './pages/Accessories';
+// import ContactUs from './pages/ContactUs';
+// import ShippingReturns from './pages/ShippingReturns';
+// import PrivacyPolicy from './pages/PrivacyPolicy';
+// import TermsConditions from './pages/TermsConditions';
+// import Product from './pages/Product';
+// import Story from './pages/Story';
+// import Packaging from './pages/Packaging';
+// import Planet from './pages/Planet';
+import Navbar from './components/Navbar';
+import './styles/output.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Navbar />
+        <Routes>
+          {/*<Route path="/all-products" component={AllProducts} />*/}
+          {/*<Route path="/tea" component={Tea} />*/}
+          {/*<Route path="/accessories" component={Accessories} />*/}
+          {/*<Route path="/pages/contact-us" component={ContactUs} />*/}
+          {/*<Route path="/pages/shipping-returns" component={ShippingReturns} />*/}
+          {/*<Route path="/pages/privacy-policy" component={PrivacyPolicy} />*/}
+          {/*<Route path="/pages/terms-conditions" component={TermsConditions} />*/}
+          {/*<Route path="/products/:id" component={Product} />*/}
+          {/*<Route path="/pages/story" component={Story} />*/}
+          {/*<Route path="/pages/packaging" component={Packaging} />*/}
+          {/*<Route path="/pages/planet" component={Planet} />*/}
+          {/*<Route path="/" component={Home} />*/}
+        </Routes>
+      </Router>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SiteIcon from '../assets/site/heroLogo.png';
+import SearchIcon from '../assets/site/searchIcon.svg';
+import CartIcon from '../assets/site/cartIcon.svg';
+
+function Navbar() {
+    return (
+        <nav className=''>
+            <div className='navWrapper flex flex-row items-center align-center justify-between border-b'>
+            <div className='flex flex-row font-bold text-xl'>
+            <div className='my-2 mx-5'>
+                <Link to="/all-products">SHOP</Link>
+            </div>
+            <div className='my-2 mx-5'>
+                <Link to="/pages/story">ABOUT</Link>
+            </div>
+            </div>
+            <div>
+                <Link to="/">
+                    <img src={SiteIcon} alt="Site Icon" />
+                </Link>
+            </div>
+            <div className='flex flex-row px-'>
+                <div>
+                    <img src={SearchIcon} alt="Search Icon" className='h-6 w-6'/>
+                </div>
+                <div className='ml-6'>
+                    <img src={CartIcon} alt="Cart Icon" className='h-6 w-6 '/>
+                </div>
+            </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
