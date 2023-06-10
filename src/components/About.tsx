b0cc86de9d3f6b220e0c5068530fcb4519ee68f6
Aaron <66452927+AaronNgoi@users.noreply.github.com>
2023-06-11 03:19:18 +1000

    About section is responsive. Fixing Nav Bar now

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import tradition from '../assets/site/storyTradition.webp'
 import packaging from '../assets/site/storyPackaging.webp'
 import planet from '../assets/site/storyPlanet.webp'
+import WOW from 'wowjs';
+
+
 
 type AboutType = {
     name: string,
@@ -14,46 +17,81 @@ type AboutType = {
 const aboutData: AboutType[] = [
     {
         name: "STORY",
-        subText: "Matcha is our passion",
-        mainText: "HONOURING TRADITION",
-        paragraph: "Yasuragi Matcha stands for tranquility and the harmonious union of mind, body, and spirit. Our matcha is meticulously crafted using time-honoured Japanese techniques, offering a taste of invigorating freshness with each sip. Sourced from organic tea gardens, each leaf is carefully selected, shade-grown, and stone-ground to a fine powder, preserving its vibrant color and potent antioxidant properties.",
+        subText: "Embrace the Tradition",
+        mainText: "TIME-HONORED CRAFTSMANSHIP",
+        paragraph: "Yasuragi Tea signifies tranquility and harmony of mind, body, and spirit. Our matcha is crafted using time-honoured Japanese techniques, offering a taste of invigorating freshness with each sip. Sourced from organic tea gardens, each leaf is carefully selected, shade-grown, and stone-ground to a fine powder, preserving its vibrant color and potent antioxidant properties.",
         src: tradition,
     },
     {
         name: "PACKAGING",
-        subText: "Sustainable from leaf to package",
+        subText: "Responsibly Sourced",
         mainText: "ECO-CONSCIOUS PACKAGING",
-        paragraph: "Yasuragi Matcha is not only focused on delivering the highest quality of matcha but also takes responsibility for the environment. Our packaging is thoughtfully designed to reduce waste and ensure sustainability. We use eco-friendly, recyclable and biodegradable materials in our packaging, making sure our love for matcha extends to our love for the planet. Our commitment to sustainability reflects in every detail from leaf to package.",
+        paragraph: "Yasuragi Tea delivers top-tier matcha with environmental responsibility in mind. Our packaging minimizes waste, employing eco-friendly, recyclable and biodegradable materials. Our respect for the planet shines through our commitment to sustainability, evident in every step from leaf to package.",
         src: packaging
     },
     {
         name: "PLANET",
-        subText: "Our commitment to Earth",
-        mainText: "CARBON NEUTRAL AND BEYOND",
-        paragraph: "At Yasuragi Matcha, we are driven by the pursuit of harmony - with our bodies, our minds, our spirit, and with our planet. We understand the importance of taking care of our environment and are committed to reducing our carbon footprint. All our deliveries are carbon neutral, as we consistently strive for more sustainable practices in all our processes. We are also active in efforts to offset our carbon emissions, thus contributing positively to the environment. Our ultimate goal is to achieve a balance where we give back to the Earth just as much as we take, ensuring a sustainable future for all.",
+        subText: "Drink Green",
+        mainText: "OUR PLANET, OUR MATCHA",
+        paragraph: "At Yasuragi Tea, we seek harmony with ourselves and the planet. We're dedicated to reducing our carbon footprint, offering only carbon-neutral deliveries and actively offsetting our emissions. Our aim is not just to minimize our environmental impact, but to create a sustainable future where we give back to the Earth as much as we receive.",
         src: planet
     },
 ];
 const About: React.FC = () => {
     const [activeTab, setActiveTab] = useState(aboutData[0].name); // default to the first tab
+    const [animate, setAnimate] = useState(false);
+
+    useEffect(() => {
+        new WOW.WOW({
+            live: false
+        }).init();
+    }, []);
+
+    const handleTabChange = (name: string) => {
+        setAnimate(true); // trigger animation
+        setActiveTab(name);
+    };
 
     const renderContent = () => {
         const activeData = aboutData.find(tab => tab.name === activeTab);
 
+
         return (
-            <div className="AboutContentWrapper flex flex-row">
-                <div className="flex flex-row justify-end">
-                    <div className='AboutImageWrapper'>
-                    <img src={activeData?.src} alt={activeData?.name} />
+            <>
+                {/*desktop*/}
+            <div className="AboutContentWrapper wow focusIn mobile-hidden tablet-hidden mb-10 lg:mb-16 flex flex-row justify-end items-end h-full">
+                <div className="flex h-full ">
+                    <div className='AboutImageWrapper h-full'>
+                    <img className={`AboutImage h-full  ${animate ? 'slowFocusIn' : ''}`} src={activeData?.src} alt={activeData?.name} />
                     </div>
                 </div>
-                <div className="AboutTextBlock p-6 h-full ">
-                    <h3 className="text-lg">{activeData?.subText}</h3>
-                    <h1 className="text-2xl font-bold AboutMainText AboutGreen">{activeData?.mainText}</h1>
-                    <p>{activeData?.paragraph}</p>
-                    <p className="AboutGreen">Discover More</p>
+                <div className={`AboutTextBlock mobile-hidden tablet-hidden p-6 lg:p-11 h-full ${animate ? 'riseUp' : ''}`} onAnimationEnd={() => setAnimate(false)}>
+
+                <h3 className=" font-bold uppercase tracking-widest py-1 mb-3">{activeData?.subText}</h3>
+                    <h1 className=" font-bold AboutMainText AboutGreen py-1 mb-3">{activeData?.mainText}</h1>
+                    <p className='mb-6'>  {activeData?.paragraph}</p>
+                    <p className="AboutGreen underline font-bold ">Discover More</p>
                 </div>
             </div>
+
+                {/*mobile*/}
+        <div className="flex h-full desktop-hidden mobileAbout">
+            <div
+                className={`AboutTextBlock flex m-auto p-6 lg:p-11 h-full ${animate ? 'focusIn' : ''}`}
+                onAnimationEnd={() => setAnimate(false)}
+            >
+                <div className='mobileTextWrapper items-center text-center justify-center wow'>
+                <h3 className="font-bold uppercase tracking-widest py-1 mb-3">{activeData?.subText}</h3>
+                <h1 className="font-bold AboutMainText AboutGreen py-1 mb-3">{activeData?.mainText}</h1>
+                <p className="mb-6">{activeData?.paragraph}</p>
+                <p className="AboutGreen underline font-bold">Discover More</p>
+                </div>
+            </div>
+            <div className="wow  AboutBgWrapper" style={{ backgroundImage: `url(${activeData?.src})` }}>
+                <div className="Overlay"></div>
+            </div>
+        </div>
+        </>
         )
     }
 
@@ -61,7 +99,11 @@ const About: React.FC = () => {
     const renderTabs = () => {
         return aboutData.map((tab) => {
             return (
-                <div onClick={() => setActiveTab(tab.name)} className={`px-4 py-2 font-bold ${activeTab === tab.name ? 'ActiveAboutTab' : 'PassiveAboutTab'}`}>
+                <div
+                    key={tab.name}
+                    onClick={() => handleTabChange(tab.name)}
+                    className={`py-2 h-12 flex flex-row items-center justify-center font-bold ${activeTab === tab.name ? 'ActiveAboutTab' : 'PassiveAboutTab'}`}
+                >
                     {tab.name}
                 </div>
             )
@@ -69,9 +111,9 @@ const About: React.FC = () => {
     }
 
     return (
-        <div className="AboutBlock lg:py-24 w-full">
+        <div className="AboutBlock lg:pt-24 lg:pb-12 w-full">
             {renderContent()}
-            <div className="flex flex-row justify-center mb-6 items-center ">
+            <div className="flex flex-row justify-center mt-4 mb-4 lg:mb-6 items-center overflow-hidden">
                 {renderTabs()}
             </div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,8 +45,8 @@ function Navbar() {
                     </div>
             }
         <nav className={navbarClasses.join(" ")}>
-            <div className='navWrapper flex flex-row items-center align-center justify-between'>
-            <div className='flex flex-row font-bold text-xlg tracking-widest'>
+            <div className='navWrapper items-center justify-between'>
+            <div className='flex flex-row font-bold text-xlg tracking-widest justify-start'>
             <div className='my-2 mx-6'>
                 <Link to="/all-products">SHOP</Link>
             </div>
@@ -54,12 +54,12 @@ function Navbar() {
                 <Link to="/pages/story">ABOUT</Link>
             </div>
             </div>
-            <div>
+                <div className="flex">
                 <Link to="/">
-                    <img src={SiteIcon} alt="Site Icon" />
+                    <img src={SiteIcon} alt="Site Icon " className='siteIcon' />
                 </Link>
             </div>
-            <div className='flex flex-row px-'>
+            <div className='flex flex-row justify-end'>
                 <div>
                     <img src={SearchIcon} alt="Search Icon" className='h-6 w-6'/>
                 </div>
