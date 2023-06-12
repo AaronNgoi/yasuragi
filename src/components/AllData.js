0a37c185cb913175756762942924961028cc8ff0
Aaron <66452927+AaronNgoi@users.noreply.github.com>
2023-06-12 14:34:51 +1000

    rename for consistency

---
+++ src/components/AllData.js
@@ -0,0 +1,7 @@
+import matchaPowder from '../assets/matchaPowder.webp';
+
+import MatchaPour from '../assets/MatchaPour.webp';
+
+import matchaGlass from '../assets/matchaGlass.webp';
+
+import matchaBottle from '../assets/matchaBottle.webp';
