c81ddbed557fc0e2973ee71330eacdee5a696422
Aaron <66452927+AaronNgoi@users.noreply.github.com>
2023-06-15 15:24:51 +1000

    Fixing mobile nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,9 @@ import desktopIcon from '../assets/site/YausragiDesktopLogo.webp';
 import SearchIcon from '../assets/site/searchIcon.svg';
 import CartIcon from '../assets/site/cartIcon.svg';
 import menuIcon from '../assets/site/menu.svg';
+import CloseIcon from '../assets/site/close.svg';
+import add from '../assets/site/add.svg';
+import minus from '../assets/site/minus.svg';
 import products from './AllData';
 import ProductPreview from './ProductPreview';
 
@@ -14,6 +17,9 @@ function Navbar() {
     const [scrolled, setScrolled] = useState(false);
     const [showAboutMegaMenu, setShowAboutMegaMenu] = useState(false);
     const [showShopMegaMenu, setShowShopMegaMenu] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [mobileTeaMenuOpen, setMobileTeaMenuOpen] = useState(false);
+    const [mobileAccessoriesMenuOpen, setMobileAccessoriesMenuOpen] = useState(false);
 
     const handleScroll = () => {
         const offset = window.scrollY;
@@ -54,6 +60,18 @@ function Navbar() {
         setShowShopMegaMenu(false);
     };
 
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen);
+    };
+
+    const toggleMobileTeaMenu = () => {
+        setMobileTeaMenuOpen(!mobileTeaMenuOpen);
+    };
+
+    const toggleMobileAccessoriesMenu = () => {
+        setMobileAccessoriesMenuOpen(!mobileAccessoriesMenuOpen);
+    };
+
 
     const teaProducts = products.filter((product) => product.type === 'tea');
     const accessoryProducts = products.filter((product) => product.type === 'accessory');
@@ -69,6 +87,80 @@ function Navbar() {
                         </div>
                     </div>
             }
+
+            {mobileMenuOpen && (
+                <div className="mobileNavMenuPopOut ">
+                    {/*<div className="mobileNavMenuBackground" onClick={toggleMobileMenu}></div>*/}
+                    <div className="mobileNavMenuCloseIcon" onClick={toggleMobileMenu}>
+                        <img src={CloseIcon} alt="Close Icon" className="h-5 w-5" />
+                    </div>
+                    <div className="mobile-accordion-menu darkBrownFont">
+                        <div className="mobile-accordion-menu-item">
+                            <div className="accordion-header " onClick={showShopMegaMenu ? hideMegaMenusHandler : showShopMegaMenuHandler}>
+                                <div className="flex justify-between items-center">
+                                    <div className=''>SHOP</div>
+                                    <div>
+                                        <img className="h-4 w-4" src={showShopMegaMenu ? minus : add} alt="Expand" />
+                                    </div>
+                                </div>
+                            </div>
+                            {showShopMegaMenu && (
+                                <ul className="accordion-content">
+
+                                    <div className="flex justify-between items-center" onClick={toggleMobileTeaMenu}>
+                                        <div className='my-2'>TEA</div>
+                                        <div>
+                                            <img className="h-4 w-4" src={mobileTeaMenuOpen ? minus : add} alt="Expand" />
+                                        </div>
+                                    </div>
+
+                                    {mobileTeaMenuOpen && teaProducts.map((product) => (
+                                        <li className='shopMegaMenuLink' key={product.id}>
+                                            <Link to={`/products/${product.id}`}>{product.title}</Link>
+                                        </li>
+                                    ))}
+
+
+
+                                    <div className="flex justify-between items-center" onClick={toggleMobileAccessoriesMenu}>
+                                        <div className='my-2'>ACCESSORIES</div>
+                                        <div>
+                                            <img className="h-4 w-4" src={mobileAccessoriesMenuOpen ? minus : add} alt="Expand" />
+                                        </div>
+                                    </div>
+
+                                    {mobileAccessoriesMenuOpen && accessoryProducts.map((product) => (
+                                        <li className='shopMegaMenuLink' key={product.id}>
+                                            <Link to={`/products/${product.id}`}>{product.title}</Link>
+                                        </li>
+                                    ))}
+
+                                    <li><Link to="/all-products" className='my-2'>VIEW ALL</Link></li>
+                                </ul>
+                            )}
+                        </div>
+                        <div className="mobile-accordion-menu-item">
+                            <div className="accordion-header " onClick={showAboutMegaMenu ? hideMegaMenusHandler : showAboutMegaMenuHandler }>
+                                <div className="flex justify-between items-center">
+                                    <div className=''>ABOUT</div>
+                                    <div>
+                                        <img className="h-4 w-4" src={showAboutMegaMenu ? minus : add} alt="Expand" />
+                                    </div>
+                                </div>
+                            </div>
+                            {showAboutMegaMenu && (
+                                <div className="accordion-content">
+                                    <ul className="flex flex-col ">
+                                        <li><Link to="/pages/story">STORY</Link></li>
+                                        <li><Link to="/pages/packaging">PACKAGING</Link></li>
+                                        <li><Link to="/pages/planet">PLANET</Link></li>
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            )}
         <nav className={navbarClasses.join(" ")} onMouseLeave={hideMegaMenusHandler}>
             <div className='navWrapper flex flex-row items-center align-center justify-between relative'>
             <div className='flex flex-row text-xlg tracking-widest items-center'>
@@ -148,10 +240,14 @@ function Navbar() {
                     </ul>
                 </div>
             </div>
-                <div className='desktop-hidden '>
+                <div className='desktop-hidden'  onClick={toggleMobileMenu}>
                     <img src={menuIcon} alt="Menu Icon" className='h-6 w-6'/>
+                    {/* Mobile Accordion Menu */}
                 </div>
             </div>
+
+
+
                 <div className="flex ">
                     <div className=''>
                 <Link to="/">
